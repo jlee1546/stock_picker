0a5e2680281c190a5a215e619c7d3021e6122a9e
Jonathan Lee <jlee1546.jl@gmail.com>
2022-08-21 11:36:37 -0400

    Factor program, choose better variable names

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,27 +1,31 @@
-array = [1, 7, 11]
+
 
 
 def stock_picker(array)
-  new_array = array.map{|element| element}
-  return_array = []
-  a = 0
-  b = 0
+  # variables for the day to buy, the day to sell, and max for maximum profit
+  buy_day = 0
+  sell_day = 0
+  max_profit = 0
   
-
-  new_array.each_with_index do |elementA, indexA|
+  # nested array for calculation
+  array.each_with_index do |elementA, indexA|
     array.each_with_index do |elementB, indexB|
-       profit = elementB - elementA
-      if !defined?(max) || profit > max
-        max = profit
-        a = indexA
-        b = indexB
-        puts "max = #{max} a = #{a} b = #{b}"
+
+      profit = elementB - elementA
+      # assign profit to max and indicies to buy/sell days when conditions met
+      if  profit > max_profit && indexA < indexB
+        max_profit = profit
+        buy_day = indexA
+        sell_day = indexB
+        
       end
     end
   end
-  
-  [a,b]
+  # returns best days to buy (a) and sell (b) as an array
+  p [buy_day,sell_day]
 end
 
+#sample array of prices
+array_of_prices = [17, 3, 6, 9, 15, 8, 6, 1, 10]
 
- print stock_picker(array)
+stock_picker(array_of_prices)
